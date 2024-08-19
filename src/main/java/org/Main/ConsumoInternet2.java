b67c29619b80162a3cd612a9890310221787ab3f
Lucas H B Medeiros <lucashenriquebarros2016@gmail.com>
2024-08-19 15:35:10 -0300

    concluido

--- src/main/java/org/Main/ConsumoInternet2.java
+++ src/main/java/org/Main/ConsumoInternet2.java
@@ -1,13 +1,34 @@
 package org.Main;
 
+import java.util.Scanner;
+
 public class ConsumoInternet2 {
 
-    public static int gbParaMb(double gb){
+    public static int gbParaMb(int gb){
+        return gb * 1024;
 
     }
 
-    public static verificarLimiteDados(double limiteGb, int consumoTotalMb) {
+    public static void verificarLimiteDados(int limiteGb, int consumoTotalMb) {
         int limitemb = gbParaMb(limiteGb);
-        
+
+        if (consumoTotalMb > limitemb) {
+            System.out.println("Limite de dados excedido. Compre ou renove seu pacote.");
+        } else {
+            int dadosRestantes = limitemb - consumoTotalMb;
+            System.out.println("Você ainda possui " + dadosRestantes + " MB disponiveis.");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        int limiteGb = entrada.nextInt();
+        entrada.nextLine();
+
+        int consumoTotalMb = entrada.nextInt();
+
+        verificarLimiteDados(limiteGb, consumoTotalMb);
     }
 }
